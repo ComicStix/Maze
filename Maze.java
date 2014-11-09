ffd04fe50f5e4e5165b44fe46002c3dcf17604f6
Nicole Daniels <ndaniels320gmail.com>
2014-11-08 19:28:48 -0500

    generates outer walls

--- Maze.java
+++ Maze.java
@@ -4,9 +4,9 @@ import java.util.Random;
 public class Maze
 {
 	// TO DO: Instance Variable
-	int height;
 	int width;
-	int [][][]board;
+	int height;
+	int[][][]board;
 	
 	/**
 	 * Constructor
@@ -17,9 +17,37 @@ public class Maze
 	{
 		width = aWidth;
 		height = aHeight;
-		board = new int [width][height][4];
+		board = new int[height][width][4];
 		if (getWidth()==1 || getHeight()==1){
 			return;
+		}
+		Random rand = new Random();
+		int randomX = rand.nextInt((getWidth()-1 - 0) + 1) + 0;
+		int randomY = rand.nextInt((getHeight()-1 -0) + 1) + 0;
+		
+		for (int i = 0; i < height; i++){
+			for(int j = 0; j < width; j++){
+				/*if (j == randomX){
+					board[i][j][1]=1;
+				}
+				if (i == randomY){
+					board[i][j][2]=1;
+				}
+				if (j == randomX + 1){
+					board[i][j][0]=1;
+				}
+				if (i == randomY+1){
+					board[i][j][3]=1;
+				}
+				*/
+				//set outside walls
+				//x= i, y = j
+				if (j == 0)board[i][j][3]=1;
+				if (i == 0)board[i][j][0]=1;
+				if(i == getHeight()-1)board[i][j][1]=1;
+				if(j == getWidth()-1)board[i][j][2]=1;
+				
+			}
 		}
 		// TO DO: Constructor
 	}
@@ -54,6 +82,11 @@ public class Maze
 	public boolean isNorthWall(int row, int column)
 	{
 		// TO DO
+		boolean isNorthWall = false;
+		if(board[row][column][0]==1){
+			isNorthWall=true;
+		}
+		return isNorthWall;
 	}
 	
 	/**
@@ -65,7 +98,11 @@ public class Maze
 	 */
 	public boolean isEastWall(int row, int column)
 	{
-		// TO DO
+		boolean isEastWall = false;
+		if(board[row][column][2]==1){
+			isEastWall = true;
+		}
+		return isEastWall;
 	}
 	
 	/**
@@ -78,6 +115,11 @@ public class Maze
 	public boolean isSouthWall(int row, int column)
 	{
 		// TO DO
+		boolean isSouthWall = false;
+		if(board[row][column][1]==1){
+			isSouthWall = true;
+		}
+		return isSouthWall;
 	}
 	
 	/**
@@ -89,6 +131,11 @@ public class Maze
 	 */
 	public boolean isWestWall(int row, int column)
 	{
+		boolean isWestWall = false;
+		if(board[row][column][3]==1){
+			isWestWall = true;
+		}
+		return isWestWall;
 		// TO DO
 	}
 }
